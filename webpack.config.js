43a02a563a0b622aa423133371820f16573ebda5
tarunbhartiya7 <tarunbhartiya7@gmail.com>
2017-12-27 12:32:00 +0530

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,15 @@ const config = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),	//requires absolute path, create a dist folder and save inside as bundle.js
 		filename: 'bundle.js'	//you can name this anything
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/
+			}
+		]
 	}
 };
 
-module.exports = config;
+module.exports = config;	
