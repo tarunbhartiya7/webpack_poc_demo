0630985d8dd5c94fa355421c431081eca28fbc43
tarunbhartiya7 <tarunbhartiya7@gmail.com>
2017-12-27 17:46:54 +0530

    add purify css - create only those css which are required for optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 const config = {
 	entry: './src/index.js',	//entry point relative path
@@ -24,6 +26,10 @@ const config = {
 	},
 	plugins: [
 		new ExtractTextPlugin('style.css'),
+		new PurifyCSSPlugin({
+			// Give paths to parse for rules. These should be absolute!
+			paths: glob.sync(path.join(__dirname, './*.html')),
+		})
 	],
 	watch: true
 };
